d8c2da5368607ad78f123252988bf787f294c209
Gavin John <gavinnjohn@gmail.com>
2026-07-24 17:17:22 -0400

    grayjay: split off grayjay-frontend; grayjay-frontend: init

---
+++ pkgs/by-name/gr/grayjay-frontend/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildNpmPackage,
+  grayjay,
+}:
+
+buildNpmPackage {
+  pname = "${grayjay.pname}-frontend";
+
+  # nixpkgs-update: no auto update
+  inherit (grayjay) version src;
+
+  sourceRoot = "source/Grayjay.Desktop.Web";
+
+  npmBuildScript = "build";
+  npmDepsHash = "sha256-3yJIPkuEvkFL9Wb4y/r0yEULQbXx/wHqicFBLzOPj68=";
+
+  installPhase = ''
+    runHook preInstall
+    cp -r dist/ $out
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Grayjay frontend subpackage";
+    inherit (grayjay.meta)
+      homepage
+      license
+      maintainers
+      platforms
+      ;
+  };
+}
