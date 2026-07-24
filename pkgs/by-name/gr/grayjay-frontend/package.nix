b64177a3c1a5d82c07013c8896070b7ac24d2c7c
Gavin John <gavinnjohn@gmail.com>
2026-07-24 17:17:22 -0400

    grayjay: split off grayjay-frontend; grayjay-frontend: init

---
+++ pkgs/by-name/gr/grayjay-frontend/package.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  buildNpmPackage,
+  grayjay,
+}:
+
+buildNpmPackage {
+  pname = "${grayjay.pname}-frontend";
+
+  # nixpkgs-update: no auto update
+  inherit (grayjay) version src;
+
+  sourceRoot = "source/Grayjay.Desktop.Web";
+
+  npmBuildScript = "build";
+  npmDepsHash = "sha256-3yJIPkuEvkFL9Wb4y/r0yEULQbXx/wHqicFBLzOPj68=";
+
+  installPhase = ''
+    runHook preInstall
+    cp -r dist/ $out
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "curl-impersonate shim used by Grayjay";
+    homepage = "https://grayjay.app/desktop/";
+    inherit (grayjay) license maintainers platforms;
+  };
+}
