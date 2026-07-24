84fbd48b88a621995918a3ea0318a65905f17b99
Gavin John <gavinnjohn@gmail.com>
2026-07-24 19:07:13 -0400

    f

--- pkgs/by-name/cu/curl-impersonate/package.nix
+++ pkgs/by-name/cu/curl-impersonate/package.nix
@@ -47,6 +47,7 @@ stdenv.mkDerivation rec {
   # warnings on `boringssl`.
   env.NIX_CFLAGS_COMPILE = "-Wno-error";
 
+  separateDebugInfo = true;
   strictDeps = true;
   __structuredAttrs = true;
 
