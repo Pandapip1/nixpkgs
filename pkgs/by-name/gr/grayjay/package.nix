2416b335da6fa35b621f5ec7ffcae6636cbcc7c4
Gavin John <gavinnjohn@gmail.com>
2026-07-24 16:47:12 -0400

    grayjay: add aarch64-linux to supported platforms

--- pkgs/by-name/gr/grayjay/package.nix
+++ pkgs/by-name/gr/grayjay/package.nix
@@ -220,7 +220,10 @@ buildDotnetModule (finalAttrs: {
       kruziikrel13
       samfundev
     ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
     mainProgram = "Grayjay";
   };
 })
