a338e71d854819e39e004ca2954237ba65ad5bcc
Gavin John <gavinnjohn@gmail.com>
2026-07-24 16:46:45 -0400

    grayjay: unvendor and fix aarch64 build

--- pkgs/by-name/gr/grayjay/package.nix
+++ pkgs/by-name/gr/grayjay/package.nix
@@ -3,6 +3,11 @@
   fetchFromGitLab,
   dotnetCorePackages,
   lib,
+  stdenv,
+  ffmpeg,
+  curl-impersonate,
+  libsodium,
+  sqlite,
   libz,
   icu,
   openssl,
@@ -39,6 +44,7 @@
   wrapGAppsHook3,
   _experimental-update-script-combinators,
   grayjay-frontend,
+  grayjay-libcurlshim,
 }:
 let
   version = "17";
@@ -71,6 +77,7 @@ buildDotnetModule (finalAttrs: {
     nss
     icu
     krb5
+    curl-impersonate
   ];
 
   nativeBuildInputs = [
@@ -126,11 +133,20 @@ buildDotnetModule (finalAttrs: {
   '';
 
   postInstall = ''
-    chmod +x $out/lib/grayjay/cef/dotcefnative
-    chmod +x $out/lib/grayjay/ffmpeg
-    rm $out/lib/grayjay/Portable
     ln -s /tmp/grayjay-launch $out/lib/grayjay/launch
     ln -s /tmp/grayjay-cef-launch $out/lib/grayjay/cef/launch
+
+    # Unvendor most stuff
+    rm -f $out/lib/grayjay/{Portable,ffmpeg,libcurl-impersonate.so,libcurlshim.so,libsodium.so,libe_sqlite3.so}
+    ln -s ${lib.getExe ffmpeg} $out/lib/grayjay/ffmpeg
+    ln -s ${curl-impersonate}/lib/libcurl-impersonate${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/grayjay/libcurl-impersonate.so
+    ln -s ${grayjay-libcurlshim}/lib/libcurlshim${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/grayjay/libcurl-impersonate.so
+    ln -s ${libsodium}/lib/libsodium${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/grayjay/libsodium.so
+    ln -s ${sqlite.out}/lib/libsqlite3${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/grayjay/libe_sqlite3.so
+
+    # CEF is still vendored for now
+    chmod +x $out/lib/grayjay/cef/dotcefnative
+
     mkdir -p $out/share/icons/hicolor/scalable/apps
     ln -s $out/lib/grayjay/grayjay.png $out/share/icons/hicolor/scalable/apps/grayjay.png
   '';
@@ -195,7 +211,10 @@ buildDotnetModule (finalAttrs: {
       samfundev
       pandapip1
     ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
     mainProgram = "Grayjay";
   };
 })
